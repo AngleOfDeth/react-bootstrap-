18b5c28962326dce8634b9672eda0427030bff60
behzad <behzad.mazaheri@yahoo.com>
2023-11-01 16:15:57 +0330

    add new navbar adn new slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
-
-
 const App = () => {
   return (
-    <div>
+    <header className='header position-relative'>
       <Navbar/>
-      <Slider/>
-    </div>
+      <section className="h-75"> <Slider /> </section>
+    </header>
   )
 }
 
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,42 @@
-import { Button } from "react-bootstrap"
-
+import Carousel from 'react-bootstrap/Carousel';
 const Slider = () => {
   return (
-    <div>
-        <Button variant="success"> click me </Button>
+    <div className='h-50' >
+      <Carousel data-bs-theme="dark">
+        <Carousel.Item >
+          <img
+            className="d-block w-100 "
+            style={{height : "95vh"}}
+            src="./images/s01.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+ 
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item >
+          <img
+            className="d-block w-100 "
+            style={{height : "95vh"}}
+            src="./images/s02.jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+          
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item >
+          <img
+            className="d-block w-100 "
+            style={{height : "95vh"}}
+            src="./images/s03.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+         
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   )
 }
