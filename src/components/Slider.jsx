c9420bb77407065a8b24bcc99f4862439874c6d5
behzad <behzad.mazaheri@yahoo.com>
2023-11-01 14:49:56 +0330

    add btn

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,36 +1,11 @@
-import Carousel from 'react-bootstrap/Carousel';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import { Button } from "react-bootstrap"
 
-function IndividualIntervalsExample() {
-    return (
-        <div style={{ height: "80vh !important " }} >
-            <Carousel>
-                <Carousel.Item interval={1000}>
-                    <img className='w-100 d-block' src='./images/s01.jpg' />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img className='w-100 d-block' src='./images/s02.jpg' />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img className='w-100 d-block' src='./images/s03.jpg' />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </div>
-    );
+const Slider = () => {
+  return (
+    <div>
+        <Button variant="success"> click me </Button>
+    </div>
+  )
 }
 
-export default IndividualIntervalsExample;
+export default Slider
